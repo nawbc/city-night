0dcf957a7036de51bc7a8feae680c1e4e3cd95c3
sewerganger <wanghan9423@outlook.com>
2019-10-23 21:37:21 +0800

    no commit message

--- bin/dev.server.js
+++ bin/dev.server.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-console */
 'use strict';
 
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const { LAUNCH_HOST, LAUNCH_PORT } = require('../scripts/utils/preHandleDev');
 const openBrowser = require('../scripts/utils/openBrowser');
+const formatMessages = require('webpack-format-messages');
 const { appPublicPath } = require('../scripts/utils/helper');
 const appConfig = require('../scripts/webpack/webpack.app');
 const chalk = require('chalk');
@@ -42,36 +44,74 @@ const serverConfig = {
 	}
 };
 
-const compile = () =>
-	new Promise((resolve, reject) => {
+const displayAfterSuccess = () => {
+	console.clear();
+	console.log(chalk.green('Silent development server running on ...\n'));
+	console.log('=====================================================\n');
+	console.log(chalk.green(`[ADDR]: ${targetAddress}`));
+	console.log();
+	console.log('[Fuck] Good luck to myself 😜');
+};
+
+const compilerHooks = compiler => {
+	compiler.hooks.done.tap('done', function(stats) {
+		const messages = formatMessages(stats);
+
+		console.clear();
+		if (messages.errors.length) {
+			console.log(chalk.red('Failed to compile.'));
+			messages.errors.forEach(e => console.log(e));
+			return;
+		}
+
+		if (!messages.errors.length && !messages.warnings.length) {
+			console.log(`${chalk.green('###')} Compiling Success ${chalk.green('###')}`);
+			displayAfterSuccess();
+		}
+		if (messages.warnings.length) {
+			console.log(chalk.yellow('Compiled with warnings.'));
+			messages.warnings.forEach(w => console.log(w));
+		}
+	});
+
+	compiler.hooks.invalid.tap('invalid', function() {
+		console.clear();
+		console.log(`${chalk.green('###')} Compiling ${chalk.green('###')}`);
+	});
+};
+
+const compile = () => {
+	return new Promise((resolve, reject) => {
 		try {
 			const config = appConfig(devAppTargetName, devAppBuildTarget);
 			const compiler = webpack(config);
+			compilerHooks(compiler);
 			resolve(compiler);
 		} catch (err) {
 			reject(err);
 		}
 	});
+};
 
-const runServer = compiled =>
-	new Promise(() => {
-		const devServer = new WebpackDevServer(compiled, serverConfig);
+const runServer = compiler => {
+	return new Promise(() => {
+		const devServer = new WebpackDevServer(compiler, serverConfig);
 		devServer.listen(LAUNCH_PORT, LAUNCH_HOST, err => {
 			err && console.log(chalk.red(err));
-			process.stdout.write(
-				process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H'
-			);
-			console.log(chalk.green('Silent development server running on ...\n'));
-			console.log(chalk.yellow(`[❤ ❤ ❤ ❤ ❤]address: ${targetAddress}`));
-			console.log();
-			console.log(chalk.blue('\t Good luck to myself 😜'));
 			openBrowser(targetAddress);
 		});
+		['SIGINT', 'SIGTERM'].forEach(signal => {
+			process.on(signal, () => {
+				devServer.close();
+				process.exit();
+			});
+		});
 	});
+};
 
 (async function() {
-	const result = await compile()
+	const compiler = await compile()
 		.then(val => val)
 		.catch(err => console.trace(chalk.red('webpack compile error', err)));
-	await runServer(result);
+	await runServer(compiler);
 })();
